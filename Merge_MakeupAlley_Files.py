fa60cc91375db4c9226066a004327a8c05cf021e
Rosanne Lai <rosannelai@gmail.com>
2017-05-28 21:56:13 -0400

    Add files via upload

--- Merge_MakeupAlley_Files.py
+++ Merge_MakeupAlley_Files.py
@@ -11,6 +11,6 @@ for x in range (1,2001):
         if os.path.getsize(f) > 0:
             df = pd.read_csv(f, index_col = None, header=None, names=None, sep='|', skiprows = 0)
             df.index = [bs(f)] * len(df)
-            df.to_csv("makeupalley_all.csv", encoding='utf8', mode='a', header=False)
+            df.to_csv("makeupalley_all.csv", encoding='utf8', mode='a', header=False, sep='\t',)
         print df
     
